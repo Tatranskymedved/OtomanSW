f2a8d2a6b49c31b5166ad5675c8e7709992e3924
Jan Urbanec <urbanecj@seznam.cz>
2016-09-07 22:52:43 +0200

    base

---
+++ TicketMaker/FormMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TicketMaker
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mBtnCreate = new System.Windows.Forms.Button();
+            this.mNumUDFrom = new System.Windows.Forms.NumericUpDown();
+            this.mNumUDTo = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mProgressBarCreated = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.mNumUDFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mNumUDTo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mBtnCreate
+            // 
+            this.mBtnCreate.Location = new System.Drawing.Point(319, 126);
+            this.mBtnCreate.Name = "mBtnCreate";
+            this.mBtnCreate.Size = new System.Drawing.Size(126, 62);
+            this.mBtnCreate.TabIndex = 0;
+            this.mBtnCreate.Text = "Create";
+            this.mBtnCreate.UseVisualStyleBackColor = true;
+            this.mBtnCreate.Click += new System.EventHandler(this.mBtnCreate_Click);
+            // 
+            // mNumUDFrom
+            // 
+            this.mNumUDFrom.Location = new System.Drawing.Point(179, 135);
+            this.mNumUDFrom.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.mNumUDFrom.Name = "mNumUDFrom";
+            this.mNumUDFrom.Size = new System.Drawing.Size(120, 20);
+            this.mNumUDFrom.TabIndex = 1;
+            this.mNumUDFrom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.mNumUDFrom.ValueChanged += new System.EventHandler(this.mNumUDFrom_ValueChanged);
+            // 
+            // mNumUDTo
+            // 
+            this.mNumUDTo.Location = new System.Drawing.Point(179, 161);
+            this.mNumUDTo.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.mNumUDTo.Name = "mNumUDTo";
+            this.mNumUDTo.Size = new System.Drawing.Size(120, 20);
+            this.mNumUDTo.TabIndex = 2;
+            this.mNumUDTo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.mNumUDTo.ValueChanged += new System.EventHandler(this.mNumUDTo_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Konec:";
+            // 
+            // mProgressBarCreated
+            // 
+            this.mProgressBarCreated.Location = new System.Drawing.Point(12, 226);
+            this.mProgressBarCreated.Name = "mProgressBarCreated";
+            this.mProgressBarCreated.Size = new System.Drawing.Size(433, 23);
+            this.mProgressBarCreated.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 261);
+            this.Controls.Add(this.mProgressBarCreated);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mNumUDTo);
+            this.Controls.Add(this.mNumUDFrom);
+            this.Controls.Add(this.mBtnCreate);
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.Text = "TicketMaker - Turbo©";
+            ((System.ComponentModel.ISupportInitialize)(this.mNumUDFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mNumUDTo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mBtnCreate;
+        private System.Windows.Forms.NumericUpDown mNumUDFrom;
+        private System.Windows.Forms.NumericUpDown mNumUDTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar mProgressBarCreated;
+    }
+}
+
---
+++ TicketMaker/FormMain.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Font = System.Drawing.Font;
+using Image = iTextSharp.text.Image;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace TicketMaker
+{
+    public partial class FormMain : Form
+    {
+        private const string Title = "TicketMaker - Turbo©";
+        private readonly List<Image> mImages;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            mImages = new List<Image>();
+        }
+
+        private void mBtnCreate_Click(object sender, EventArgs e)
+        {
+            mImages.Clear();
+            try
+            {
+                var lPath = "Vstupenka2016otoWoMan.pdf";
+                var lOriginFile = "vstupenka2016otoWoMan.jpg";
+
+                var lImage = System.Drawing.Image.FromFile(lOriginFile);
+
+                for (var i = mNumUDFrom.Value; i <= mNumUDTo.Value; i++)
+                {
+                    this.UpdateProgress(i, mNumUDTo.Value);
+
+                    var nCopyOfImage = (System.Drawing.Image) lImage.Clone();
+                    DrawNumber(i, ref nCopyOfImage);
+                    mImages.Add(Image.GetInstance(nCopyOfImage, ImageFormat.Jpeg));
+                    //mImages.Add(Image.GetInstance(nCopyOfImage, BaseColor.PINK));
+                }
+
+                var lFS = new FileStream(lPath, FileMode.Create);
+                var lDoc = new Document();
+                var lWriter = PdfWriter.GetInstance(lDoc, lFS);
+                lDoc.SetPageSize(new Rectangle(3030, 3508));
+                lDoc.Open();
+                foreach (var item in mImages)
+                {
+                    lDoc.Add(item);
+                }
+                lDoc.Close();
+
+                Process.Start(lPath);
+
+                this.UpdateProgress(-1, -1);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(
+                    "Neco se nezdarilo! Pravdepodobne mate otevreny soubor, do ktereho se maji vstupenky ulozit.\nZobrazte toto prosím autorovi aplikace:\n" +
+                    exc);
+            }
+        }
+
+
+        private void DrawNumber(decimal aNumber, ref System.Drawing.Image aImage)
+        {
+            var lGraphics = Graphics.FromImage(aImage);
+
+            TextRenderer.DrawText(lGraphics,
+                aNumber.ToString("000"),
+                new Font("Comic Sans MS", 90f, FontStyle.Regular, GraphicsUnit.Point),
+                new Point(1502, 30),
+                Color.Black);
+        }
+
+        private void mNumUDFrom_ValueChanged(object sender, EventArgs e)
+        {
+            if (mNumUDFrom.Value > mNumUDTo.Value)
+            {
+                mNumUDFrom.Value = 0;
+            }
+        }
+
+        private void mNumUDTo_ValueChanged(object sender, EventArgs e)
+        {
+            if (mNumUDFrom.Value > mNumUDTo.Value)
+            {
+                mNumUDTo.Value = 0;
+            }
+        }
+
+        private void UpdateProgress(decimal aCurrentValue, decimal aMaxValue)
+        {
+            if (aCurrentValue == -1)
+            {
+                this.mProgressBarCreated.Value = 0;
+                this.Text = Title;
+                return;
+            }
+
+            this.Text = Title + " => " + aCurrentValue + " of " + aMaxValue;
+
+            this.mProgressBarCreated.Maximum = Convert.ToInt32(aMaxValue);
+            this.mProgressBarCreated.Value = Convert.ToInt32(aCurrentValue);
+        }
+    }
+}
